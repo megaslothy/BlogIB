daae7a0de9dc845271faba3307951102df2a36f2
Chineson <gitosis@ideasbrillantes.org>
2012-01-27 20:19:30 +0100

    primera subida

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,8 @@
+class WelcomeController < ApplicationController
+  def index
+  end
+  
+  def carlos
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demoblogib::Application.config.secret_token = 'c403d7b4d935b7efcc57ac9dcb59aae30085644e42b9e80156b905f049297ee3d14c8404f6e1ef65222e95cba2ce15361b19bb47f710a32e8536ccb2bce67238'
